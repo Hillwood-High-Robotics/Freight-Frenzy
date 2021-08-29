96933663f51ceca62b6ff35b574d4b8cf54d9510
Owen Rummage <owen@rummage.dev>
2021-08-29 00:22:19 -0500

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JoystickControl.java
@@ -0,0 +1,194 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/*
+ * Test the dashboard gamepad integration.
+ */
+@Autonomous
+public class JoystickControl extends LinearOpMode {
+
+    private DcMotor right;
+    private DcMotor left;
+
+    public static final String VUFORIA_LICENSE_KEY = "AYAjvln/////AAABmdl8PQbwoELHgN1xDrvdpaYKYHPPZ9h6xxAlaTcD6R9O77TtjJz0KH3+1ePQ/sC6eHb4k/MTg1YmsNhZ7OUUWxym0Wk3mS0oxEAWQYxBAfAKRu/7EuyUf6OiQ1+Yf6RFnUN5Z7ryxJEwBrLz78+8mTwi/s1jfS5m3ZatY4OLqpfNg5jGm6zv2d7TQVwFQ6sbSA3BsVgi1LBXEfV9W3FtFXl0dRXPd3JRtL4D4ilfuISGEv8Rt+BhUg63uNtzp39fl9Am2NY942aXaRB4Zv5XfuD4klrAkZmBfk8wPTYko5D+Umec+JlxxK82NHJf/cPncNYXEqbLByr5i/Vu+ONqSaWIpBc7wKYfCOvHWAQ67P+u";
+    OpenCvCamera webcam;
+
+
+    private static void logGamepad(Telemetry telemetry, Gamepad gamepad, String prefix) {
+        telemetry.addData(prefix + "Synthetic",
+                gamepad.getGamepadId() == Gamepad.ID_UNASSOCIATED);
+        for (Field field : gamepad.getClass().getFields()) {
+            if (Modifier.isStatic(field.getModifiers())) continue;
+
+            try {
+                telemetry.addData(prefix + field.getName(), field.get(gamepad));
+            } catch (IllegalAccessException e) {
+                // ignore for now
+            }
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+        right = hardwareMap.dcMotor.get("right");
+        left = hardwareMap.dcMotor.get("left");
+
+
+        right.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+        if (isStopRequested()) return;
+        msStuckDetectStop = 2500;
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        webcam.setPipeline(new SamplePipeline());
+
+        /*
+         * Open the connection to the camera device. New in v1.4.0 is the ability
+         * to open the camera asynchronously, and this is now the recommended way
+         * to do it. The benefits of opening async include faster init time, and
+         * better behavior when pressing stop during init (i.e. less of a chance
+         * of tripping the stuck watchdog)
+         *
+         * If you really want to open synchronously, the old method is still available.
+         */
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                */
+                webcam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+            }
+        });
+
+
+        FtcDashboard.getInstance().startCameraStream(webcam, 0);
+
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+            logGamepad(telemetry, gamepad1, "gamepad1");
+            logGamepad(telemetry, gamepad2, "gamepad2");
+
+            telemetry.addData("motor_left_speed", left.getPower());
+            telemetry.addData("motor_right_speed", right.getPower());
+
+            left.setPower(gamepad2.left_stick_y);
+            right.setPower(gamepad2.right_stick_y);
+
+            telemetry.update();
+        }
+    }
+
+    class SamplePipeline extends OpenCvPipeline
+    {
+        boolean viewportPaused;
+
+        /*
+         * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
+         * highly recommended to declare them here as instance variables and re-use them for
+         * each invocation of processFrame(), rather than declaring them as new local variables
+         * each time through processFrame(). This removes the danger of causing a memory leak
+         * by forgetting to call mat.release(), and it also reduces memory pressure by not
+         * constantly allocating and freeing large chunks of memory.
+         */
+
+        @Override
+        public Mat processFrame(Mat input)
+        {
+            /*
+             * IMPORTANT NOTE: the input Mat that is passed in as a parameter to this method
+             * will only dereference to the same image for the duration of this particular
+             * invocation of this method. That is, if for some reason you'd like to save a copy
+             * of this particular frame for later use, you will need to either clone it or copy
+             * it to another Mat.
+             */
+
+            /*
+             * Draw a simple box around the middle 1/2 of the entire frame
+             */
+            Imgproc.rectangle(
+                    input,
+                    new Point(
+                            input.cols()/4,
+                            input.rows()/4),
+                    new Point(
+                            input.cols()*(3f/4f),
+                            input.rows()*(3f/4f)),
+                    new Scalar(0, 255, 0), 4);
+
+            /**
+             * NOTE: to see how to get data from your pipeline to your OpMode as well as how
+             * to change which stage of the pipeline is rendered to the viewport when it is
+             * tapped, please see {@link PipelineStageSwitchingExample}
+             */
+
+            return input;
+        }
+
+        @Override
+        public void onViewportTapped()
+        {
+            /*
+             * The viewport (if one was specified in the constructor) can also be dynamically "paused"
+             * and "resumed". The primary use case of this is to reduce CPU, memory, and power load
+             * when you need your vision pipeline running, but do not require a live preview on the
+             * robot controller screen. For instance, this could be useful if you wish to see the live
+             * camera preview as you are initializing your robot, but you no longer require the live
+             * preview after you have finished your initialization process; pausing the viewport does
+             * not stop running your pipeline.
+             *
+             * Here we demonstrate dynamically pausing/resuming the viewport when the user taps it
+             */
+
+            viewportPaused = !viewportPaused;
+
+            if(viewportPaused)
+            {
+                webcam.pauseViewport();
+            }
+            else
+            {
+                webcam.resumeViewport();
+            }
+        }
+    }
+}
